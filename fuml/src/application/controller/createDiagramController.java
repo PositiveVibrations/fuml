3375060ee5112a02f92898864e824e7dc65a67ed
Jeffrey <Jeffrey@DESKTOP-8TT30O4>
2022-04-19 14:54:47 -0500

    messing with the upload files added model package...will be adding functions there to operate the controller class for diagram and extract the methods and varaibles...

--- fuml/src/application/controller/createDiagramController.java
+++ fuml/src/application/controller/createDiagramController.java
@@ -1,8 +1,12 @@
 package application.controller;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class createDiagramController {
@@ -36,15 +41,33 @@ public class createDiagramController {
     }
     
     @FXML
+    //write the list to list.txt...will pass to functions in model later
     void Generate(ActionEvent event) throws IOException{
+        ArrayList<String> fileList = new ArrayList<String>(); 
+        for(int i = 0; i < listedFile.getItems().size(); i++){
+              fileList.add(listedFile.getItems().get(i));
+        }
+        File file = new File("src/list.txt");
+       
+        for(int i = 0; i < fileList.size(); i++){
+            try {
+                java.io.FileWriter fw = new java.io.FileWriter(file, true);
+                fw.write(fileList.get(i) + "\n");
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
+
     
+
     // returns user to home page
     @FXML
     void returnHome(ActionEvent event) throws IOException{
-    	URL url = new File("src/application/view/Main.fxml").toURI().toURL();
-    	mainPane = FXMLLoader.load(url);
+        URL url = new File("src/application/view/Main.fxml").toURI().toURL();
+        mainPane = FXMLLoader.load(url);
         Scene scene = new Scene(mainPane);// pane you are GOING TO show
         Stage window = (Stage) menuBar.getScene().getWindow();// pane you are ON
         window.setScene(scene);
@@ -53,7 +76,16 @@ public class createDiagramController {
 
     @FXML
     void uploadFile(ActionEvent event) throws IOException{
+    	
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Open Resource File");
+      
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Java files (*.java)", "*.java");
+        fc.getExtensionFilters().add(extFilter);
+      
+        File file = fc.showOpenDialog(null);
+   
+        listedFile.getItems().add(file.getPath()+file.getName());
 
-    }
-
+        }
 }
---
+++ fuml/src/application/model/createDiagram.java
@@ -0,0 +1,14 @@
+package application.model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class createDiagram {
+
+
+    }
+
+
