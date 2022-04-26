c44ff770aefb1ec700c05c982b54970665459d65
Jeffrey <Jeffrey@DESKTOP-8TT30O4>
2022-04-26 18:16:49 -0500

    removed unused imports

--- fuml/src/application/controller/createDiagramController.java
+++ fuml/src/application/controller/createDiagramController.java
@@ -10,17 +10,13 @@ import application.model.createDiagram;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
--- fuml/src/application/controller/viewDiagramsController.java
+++ fuml/src/application/controller/viewDiagramsController.java
@@ -12,11 +12,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class viewDiagramsController {
